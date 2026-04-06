375eb9f0bfe05e2b3ba92748b0ad2eef4a1754bb
Richard Rodger <richard@ricebridge.com>
2026-04-06 15:32:25 +0100

    doc-fixes

--- project/standard/.sdk/src/Top.ts
+++ project/standard/.sdk/src/Top.ts
@@ -1,48 +1,13 @@
 
 
 import {
-  cmp, each, Line, File, Content
+  cmp,
+  ReadmeTop,
 } from '@voxgig/sdkgen'
 
 
-import { KIT } from './Root'
-
-
 const Top = cmp(function Top(props: any) {
-  const { ctx$ } = props
-  const { model } = ctx$
-
-  File({ name: 'README.md' }, () => {
-    Content(`# ${model.Name} SDKs
-
-## API Entities
-
-\`\`\`mermaid 
-flowchart LR
-`)
-
-    const entityMap = model.main[KIT].entity
-
-    each(entityMap, (entity: any) => {
-      const ancestors = entity.ancestors || []
-      if (0 < ancestors.length) {
-        const pname = ancestors[ancestors.length - 1]
-        const parent = entityMap[pname]
-        if (null != parent) {
-          Line(`  ${parent.Name} --> ${entity.Name}`)
-        }
-      }
-    })
-
-    Content(`
-\`\`\`
-
-`)
-  })
-
-
-
-
+  ReadmeTop({})
 })
 
 
